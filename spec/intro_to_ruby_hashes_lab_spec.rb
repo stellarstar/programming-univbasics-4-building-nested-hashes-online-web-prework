1d75b00e5d9cfe0d6e42d73f3f672a75e79366a5
victor tim <technicalpro@securemedia.ch>
2020-01-27 16:39:48 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   :railroads=>4
   {
     :pieces=>4,
-    :rent_in_dollars=>4
+    :rent_in_dollars=>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
