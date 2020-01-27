c53de89554050ed4901467e4e504c30d5629cbae
victor tim <technicalpro@securemedia.ch>
2020-01-27 16:01:49 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -8,7 +8,7 @@ def base_hash
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200}
+      :four_pieces_owned=>200}}}
 end
 
 def monopoly_with_second_tier
