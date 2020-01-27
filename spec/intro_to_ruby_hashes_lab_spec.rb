6a55337dc8139aff4b4db5ddb030424afea12e4c
victor tim <technicalpro@securemedia.ch>
2020-01-27 17:47:05 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -26,18 +26,10 @@ def monopoly_with_third_tier
     },
     :names=>
     {
-      :reading_railroad=>
-      {
-      },
-      :pennsylvania_railroad=>
-      {
-      },
-      :b_and_o_railroad=>
-      {
-      },
-      :shortline=>
-      {
-      },
+      :reading_railroad=>{},
+      :pennsylvania_railroad=>{},
+      :b_and_o_railroad=>{},
+      :shortline=>{},
     }
   }
 }
